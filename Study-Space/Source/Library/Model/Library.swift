293e8eea53c4f3cd4b58b3b1197a1ea20b3be8d5
Jared Drueco <drueco03@gmail.com>
2024-09-14 05:18:08 -0400

    feat: implement MVVM architecture

---
+++ Study-Space/Source/Library/Model/Library.swift
@@ -0,0 +1,8 @@
+//
+//  Library.swift
+//  Study-Space
+//
+//  Created by Jared Drueco on 2024-09-14.
+//
+
+import Foundation
---
+++ Study-Space/Source/Library/View/LibraryView.swift
@@ -0,0 +1,8 @@
+//
+//  LibraryView.swift
+//  Study-Space
+//
+//  Created by Jared Drueco on 2024-09-14.
+//
+
+import Foundation
